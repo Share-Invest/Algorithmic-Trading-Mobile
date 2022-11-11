eed3b999486a2e1116989043794b6aee72cacf42
SANGWOO PARK <cyberprophet@hanmail.net>
2022-11-12 02:59:58 +0900

    초기 커밋.

---
+++ Algorithmic.Mobile/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace ShareInvest;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+    async void OnOpenWebButtonClicked(object sender, EventArgs e)
+    {
+        await Browser.OpenAsync("https://www.devexpress.com/maui/");
+    }
+}
---
+++ Algorithmic.Mobile/MauiProgram.cs
@@ -0,0 +1,26 @@
+﻿using DevExpress.Maui;
+
+using Microsoft.Maui.Controls.Compatibility.Hosting;
+
+namespace ShareInvest;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+
+        builder
+            .UseMauiApp<App>()
+            .UseDevExpress()
+            .UseMauiCompatibility()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("univia-pro-regular.ttf", "Univia-Pro");
+                fonts.AddFont("roboto-bold.ttf", "Roboto-Bold");
+                fonts.AddFont("roboto-regular.ttf", "Roboto");
+            });
+        return builder.Build();
+    }
+}
